9923e3fe3e5edcd6ab6cd4cfc1776fe8b765b079
Wayne Fisher <wayne@fisherlea.com>
2019-01-08 14:27:58 -0500

    Updated the inline JS documentation.

--- www/HeadsetControl.js
+++ www/HeadsetControl.js
@@ -1,25 +1,60 @@
 var exec = require('cordova/exec');
 
+/** 
+ * Cordova plugin for control of Bluetooth headsets.
+ *
+ * @class Cordova plugin for control of Bluetooth headsets.
+ */
 function HeadsetControl() {
-    /** Fired for all events. */
+    /** Fired for all events.
+     * @type function
+     */
     this.onevent = null;
 
-    /** Fired for events related to connecting/disconnecting of devices. */
+    /** Fired for events related to the in process connection of a device.
+     *  May not be called for all devices.
+     * @type function
+     */
     this.onconnecting = null;
+
+    /** Fired for events related to the pending connection of a device.
+     * @type function
+     */
     this.onconnect = null;
+
+    /** Fired for events related to the in process disconnection of a device.
+     *  May not be called for all devices.
+     * @type function
+     */
     this.ondisconnecting = null;
+
+    /** Fired for events related to the pending disconnection of a device.
+     * @type function
+     */
     this.ondisconnect = null;
 
-    /** Fired as a response to connect/disconnect calls. */
+    /** Fired for events related to the completed connection of a device.
+     * @type function
+     */
     this.onconnected = null;
+
+    /** Fired for events related to the completed disconnection of a device.
+     * @type function
+     */
     this.ondisconnected = null;
 
-    /** Fired for errors. */
+    /** Fired for errors.
+     * @type function
+     */
     this.onerror = null;
 
     this._init();
 }
 
+/**
+ * Initialize the plugin.
+ * @private
+ */
 HeadsetControl.prototype._init = function () {
     var that = this;
 
@@ -77,14 +112,29 @@ HeadsetControl.prototype._init = function () {
     }, "HeadsetControl", "init", []);
 };
 
+/**
+ * Connect to the headset. Preference is given to Bluetooth devices.
+ * @param {function} [success] The success callback.
+ * @param {function} [failure] The failure callback.
+ */
 HeadsetControl.prototype.connect = function (success, failure) {
     exec(success, failure, "HeadsetControl", "connect", []);
 };
 
-HeadsetControl.prototype.detect = function (success, failure) {
-    exec(success, failure, "HeadsetControl", "detect", []);
+/**
+ * Get the status of the plugin and the device connection.
+ * @param {function} [success] The success callback. The status object is passed in as the first parameter.
+ * @param {function} [failure] The failure callback.
+ */
+HeadsetControl.prototype.getStatus = function (success, failure) {
+    exec(success, failure, "HeadsetControl", "getStatus", []);
 };
 
+/**
+ * Disconnect from the headset.
+ * @param {function} [success] The success callback.
+ * @param {function} [failure] The failure callback.
+ */
 HeadsetControl.prototype.disconnect = function (success, failure) {
     exec(success, failure, "HeadsetControl", "disconnect", []);
 };
